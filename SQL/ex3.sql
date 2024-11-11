4fddf0268388aebe67ecc8b8383dd59928632f6c
sukhman <ssunner5@uwo.ca>
2024-11-11 13:59:51 -0500

    adhere to ISO

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -1,15 +1,13 @@
--- added ignore to avoid duplicates
-
-INSERT IGNORE INTO book (ISBN, title, publishedDate) 
+INSERT INTO book (ISBN, title, publishedDate) 
 VALUES ('9780062420701', 'To Kill A Mockingbird', '1960-07-11');
 
-INSERT IGNORE INTO book (ISBN, title, publishedDate)
+INSERT INTO book (ISBN, title, publishedDate)
 VALUES 
     ('9781416980063', 'Dork Diaries', '2009-06-02'),
 	('9780439064873', 'Harry Potter and the Chamber of Secrets', '1998-06-02'),
 	('9780439559818', 'Geronimo Stilton: The Curse of the Cheese Pyramid', '2004-07-01');
     
-INSERT IGNORE INTO book (ISBN, title, publishedDate)
+INSERT INTO book (ISBN, title, publishedDate)
 SELECT * FROM (
     SELECT '9780359199259', 'Murder on the Orient Express', '1934-01-01'
     UNION ALL
